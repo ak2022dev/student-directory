cf411ce959c75c4100f3c2f582d7de75721d0d0b
Amir Khan <ak2022dev@gmail.com>
2022-05-07 12:12:24 +0100

    Added printing in columns, entering cohort month with default and input checking

--- directory_interactive_opts.rb
+++ directory_interactive_opts.rb
@@ -1,78 +1,105 @@
-
-def input_students
-  puts "Please enter the names of the students"
-  puts "To finish, just hit return twice"
+def input_students_and_cohorts
   # create an empty array
   students = []
-  # get the first name
-  name = gets.chomp
-  # while the name is not empty, repeat this code
-  while !name.empty? do
-    # add the student hash to the array
-    students << {name: name, cohort: :november}
-    puts "Now we have #{students.count} students"
-    # get another name from the user
+  # output blank line
+  puts ""
+  while true
+    puts "Please enter the names and cohorts of the students"
+    puts "To finish, type stop"
     name = gets.chomp
+    if name == "stop"
+      break
+    end
+    unless name.empty?
+      # Get the cohort next and use default if none given
+      cohort = input_cohort
+      # add the student hash to the array
+      students << {name: name, cohort: cohort}
+      puts ""
+      puts "Now we have #{students.count} students"
+      puts ""
+    end    
   end
   # return the array of students
   students
 end
 
-def print_header
-  puts "The students of Villains Academy"
-  puts "-------------"
+def input_cohort
+  # output blank line
+  puts ""
+  while true
+    puts "Please input the month for cohort (default #{DEFAULT_COHORT})"
+    cohort = gets.chomp
+    if cohort.empty?
+      return DEFAULT_COHORT
+    else
+      cohort = cohort.downcase 
+      if valid_cohort(cohort)
+        return cohort.intern
+      end
+    end
+  end
 end
 
-def print(students)
-  students.each_with_index do |student, index|
-    puts "#{index+1}. #{student[:name]} (#{student[:cohort]} cohort)"
-  end
+def valid_cohort(cohort)
+  COHORT_STRINGS.include?(cohort)
+end
+
+def print_padded_header
+  # output blank line
+  puts ""
+  puts "The students of Villains Academy"
+  puts "--------------------------------"
+  print "Num".center(NUMBER_WIDTH)
+  print "Name".center(NAME_WIDTH)
+  puts "Cohort".center(COHORT_WIDTH)
 end
 
 def print_footer(students)
+  # output blank line
+  puts ""
   puts "Overall, we have #{students.count} great students"
 end
 
-def print_students_with_initial(students, letter)
-  students.each_with_index do |student, index|
-    if student[:name][0].casecmp?(letter) 
-      puts "#{index+1}. #{student[:name]} (#{student[:cohort]} cohort)"
+def print_padded_students_initial_short_names(students, letter, length_less_than)
+    i = 0
+    while i < students.length
+      if students[i][:name][0].casecmp?(letter) && students[i][:name].length < length_less_than 
+        # puts "#{i+1}. #{students[i][:name]} (#{students[i][:cohort]} cohort)"
+        print "#{i+1}".center(NUMBER_WIDTH)
+        print "#{students[i][:name]}".center(NAME_WIDTH)
+        puts "#{students[i][:cohort]}".center(COHORT_WIDTH)
+      end
+      i += 1
     end
-  end
 end
-
-def print_students_with_initial_short_names(students, letter, length_less_than)
-
-#  students.each_with_index do |student, index|
-#    if student[:name][0].casecmp?(letter) && student[:name].length < length_less_than 
-#      puts "#{index+1}. #{student[:name]} (#{student[:cohort]} cohort)"
-#    end
-#  end
-  i = 0
-  while i < students.length
-    if students[i][:name][0].casecmp?(letter) && students[i][:name].length < length_less_than 
-      puts "#{i+1}. #{students[i][:name]} (#{students[i][:cohort]} cohort)"
-    end
-    i += 1
-  end
-end
-
+  
 def get_student_initial
+  # output blank line
+  puts ""
   while true
     puts "Please enter initial of students whose names you'd like to print"
-    initial = gets.chomp[0]
+    initial = gets.chomp
     unless initial.empty?
+      initial = initial[0]
       break
     end
   end
   initial
 end
 
-# nothing happens until we call the methods
+# set some constants
 NAME_LENGTH_LESS_THAN = 12
-students = input_students
-print_header
+NUMBER_WIDTH = 6
+NAME_WIDTH = 14
+COHORT_WIDTH = 10
+DEFAULT_COHORT = :november
+COHORT_STRINGS = [ "january", "february", "march", "april",
+                   "may", "june", "july", "august", "september",
+                   "october", "november", "december" ]
+# nothing happens until we call the methods
+students = input_students_and_cohorts
 initial = get_student_initial
-# print(students)
-print_students_with_initial_short_names(students, initial, NAME_LENGTH_LESS_THAN)
+print_padded_header
+print_padded_students_initial_short_names(students, initial, NAME_LENGTH_LESS_THAN)
 print_footer(students)
